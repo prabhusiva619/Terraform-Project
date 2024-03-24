7ef30f2e5a3ae60840465d7c541bf573b9569c36
Prabhu Sivakumar <prabhusiva619@gmail.com>
2024-03-24 20:48:12 +0530

    More cleanup

--- sg.tf
+++ sg.tf
@@ -10,15 +10,6 @@ resource "aws_security_group" "elb-sg" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
-  
-#   ingress {
-#     description      = "Allow https request from anywhere"
-#     protocol         = "tcp"
-#     from_port        = 443
-#     to_port          = 443
-#     cidr_blocks      = ["0.0.0.0/0"]
-#     ipv6_cidr_blocks = ["::/0"]
-#   }
 
   egress {
     from_port   = 0
@@ -47,4 +38,5 @@ resource "aws_security_group" "ec2-sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
 }
